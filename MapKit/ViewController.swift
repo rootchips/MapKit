324f6097f22fc0fb0a920943bfc642fee4d0a136
xchpsx <roslansaidi@ymail.com>
2015-06-29 02:38:19 +0800

    MapKit
    Map View Annotation

--- MapKit/ViewController.swift
+++ MapKit/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 1
+        let location = CLLocationCoordinate2D(
+            latitude: 4.210484,
+            longitude: 101.975766
+            
+            
+        )
+        
+        
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Hello"
+        annotation.subtitle = "Im Here."
+        
+        mapView.addAnnotation(annotation)
+        
+        
+
+    
     }
 
     override func didReceiveMemoryWarning() {
